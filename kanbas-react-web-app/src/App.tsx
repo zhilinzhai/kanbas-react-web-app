034a3785979bdc52fac0e482e69ab2d61bbb95c5
zhilinzhai <zz2702@tc.columbia.edu>
2024-05-16 18:45:30 -0400

    Lab1 complete

--- kanbas-react-web-app/src/App.tsx
+++ kanbas-react-web-app/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 function App() {
   return (
+
     <div className="App">
+      <HashRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Navigate to="Labs" />} />
+            <Route path="/Labs/*" element={<Labs />} />
+          </Routes>
+        </div>
+      </HashRouter>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
