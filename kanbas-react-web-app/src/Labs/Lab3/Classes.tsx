10e2fa2f79831e90b1d7b1b2e05f1af3b5e753cd
zhilinzhai <zz2702@tc.columbia.edu>
2024-06-20 20:25:25 -0400

    first commit

---
+++ kanbas-react-web-app/src/Labs/Lab3/Classes.tsx
@@ -0,0 +1,12 @@
+import './Classes.css';
+export default function Classes() {
+  return (
+    <div>
+      <h2>Classes</h2>
+      <div className="wd-bg-yellow wd-fg-black wd-padding-10px">
+        Yellow background  </div>
+      <div className="wd-bg-blue wd-fg-black wd-padding-10px">
+        Blue background    </div>
+      <div className="wd-bg-red wd-fg-black wd-padding-10px">
+        Red background     </div><hr/>
+    </div> ) };
--- kanbas-react-web-app/src/Labs/Lab3/index.tsx
+++ kanbas-react-web-app/src/Labs/Lab3/index.tsx
@@ -1,7 +1,63 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoList from "./todos/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
 export default function Lab3() {
     return (
       <div>
         <h2>Lab 3</h2>
+        <VariablesAndConstants/>
+        <VariableTypes/>
+        <BooleanVariables/>
+        <IfElse/>
+        <TernaryOperator/>       
+        <ConditionalOutputIfElse/>
+        <ConditionalOutputInline/>
+        <LegacyFunctions/>
+        <ArrowFunctions/>
+        <ImpliedReturn/>
+        <TemplateLiterals/>
+        <SimpleArrays/>
+        <ArrayIndexAndLength/>
+        <AddingAndRemovingToFromArrays/>
+        <ForLoops/>
+        <MapFunction/>
+        <FindFunction/>
+        <FindIndex/>
+        <FilterFunction/>
+        <JsonStringify/>
+        <House/>
+        <TodoList/>
+        <Spreading/>
+        <Destructing/>
+        <FunctionDestructing/>
+        <DestructingImports/>
+        <Classes/>
+
+
       </div>
     );
   }
